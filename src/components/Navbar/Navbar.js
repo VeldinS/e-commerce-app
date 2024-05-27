a175dbb9c86374a1f91f4956fbaed4dac1ebb3e8
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-27 21:08:58 +0200

    Landing section mobile fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
     return (
         <nav className={'flex flex-row justify-between items-center px-4 lg:px-48 pt-4 pb-4 bg-light-gray absolute top-0 w-full z-50 overflow-hidden'}>
             <Link href={'/'}>
-                <Image src={logo} alt={'Logo'} />
+                <Image src={logo} alt={'Logo'}/>
             </Link>
             <div className={`lg:hidden ${isMenuOpen ? 'block' : 'hidden'}`}> {/* Conditionally render menu on small screens */}
                 <div className="flex flex-col gap-4 mt-4">
@@ -36,10 +36,10 @@ function Navbar() {
                 <Navlink href={'/'} text={'CATEGORY 3'} color={'text-black'} bgColor={'bg-black'} />
                 <Navlink href={'/'} text={'CATEGORY 4'} color={'text-black'} bgColor={'bg-black'} />
             </div>
-            <ButtonSecondary text={"Login"} />
-            <div className="lg:hidden"> {/* Conditionally render hamburger icon on small screens */}
+            <ButtonSecondary text={"PRODUCTS"} />
+            <div className="lg:hidden w-[51px] flex items-end justify-end"> {/* Conditionally render hamburger icon on small screens */}
                 <button onClick={toggleMenu}>
-                    {isMenuOpen ? <XIcon className="h-6 w-6 text-black" /> : <MenuIcon className="h-6 w-6 text-black" />}
+                    {isMenuOpen ? <XIcon className="h-7 w-7 text-black" /> : <MenuIcon className="h-7 w-7 text-black" />}
                 </button>
             </div>
         </nav>
