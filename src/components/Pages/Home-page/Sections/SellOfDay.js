da5ea583c5a7aa949a6399569c10df0928acb2d5
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-27 20:30:18 +0200

    Sell of day responsiveness implemented.

--- src/components/Pages/Home-page/Sections/SellOfDay.js
+++ src/components/Pages/Home-page/Sections/SellOfDay.js
@@ -9,13 +9,13 @@ import Link from "next/link";
 
 function SellOfDay() {
     return (
-        <section className={'bg-[#F2DEDF] h-auto py-24 px-48 flex relative flex-row justify-between items-start gap-8 overflow-hidden'}>
-            <div className={'w-2/5 flex flex-col items-start justify-start gap-6'}>
-                <h1 className={'font-audiowide text-black text-5xl leading-normal'}>
+        <section className={'bg-[#F2DEDF] h-auto py-24 xl:px-48 lg:px-24 px-4 flex relative lg:flex-row flex-col justify-between lg:items-start items-center gap-8 overflow-hidden'}>
+            <div className={'lg:w-2/5 w-full flex flex-col lg:items-start items-center justify-start lg:gap-6 gap-2'}>
+                <h1 className={'font-audiowide text-black text-center lg:text-start lg:text-5xl text-4xl leading-normal'}>
                     Sell of the Day
                 </h1>
-                <div className={'flex flex-col items-start justify-center gap-8 w-3/5'}>
-                    <p className={'font-roboto text-black text-xl leading-normal'}>
+                <div className={'flex flex-col lg:items-start items-center justify-center lg:gap-8 gap:2 lg:w-3/5 w-full'}>
+                    <p className={'font-roboto text-black lg:text-xl text-md leading-normal text-center lg:text-start'}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
                         adipiscing elit.
                     </p>
@@ -25,7 +25,7 @@ function SellOfDay() {
                     </Link>
                 </div>
             </div>
-            <Image src={product1} alt={'Product example text.'} className={'absolute bottom-0 right-[15%] md:h-2/3 sm:h-1/3 w-auto'}/>
+            <Image src={product1} alt={'Product example text.'} className={'lg:absolute relative bottom-0 lg:right-[15%] right-0 md:h-2/3 sm:h-1/3 w-auto'}/>
             <SquareImage className="absolute left-[-30px] top-[50%]" />
             <SquareImage className="absolute right-[-30px] -bottom-[20%]" />
             <SquareImage className="absolute right-[50%] top-[30%]" />
